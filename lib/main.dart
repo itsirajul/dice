d9c9da4f9b8c91a352185a0a27e3b73f6cd34a09
itsirajul <shirajulislamparvez@gmail.com>
2022-02-04 21:32:51 +0600

    Delete Container

--- lib/main.dart
+++ lib/main.dart
@@ -41,14 +41,7 @@ class DicePage extends StatelessWidget {
                 child: Image.asset('images/dice2.png')
             ),
           ),
-          Expanded(
-            child: FlatButton(
-                onPressed: () {
-                  print('Left Button Got Pressed ');
-                },
-                child: Image.asset('images/dice2.png')
-            ),
-          ),
+
 
 
         ],
