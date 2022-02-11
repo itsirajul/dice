63ccafa2a14344f9daafe0885bd6f3d381ed4d0b
itsirajul <shirajulislamparvez@gmail.com>
2022-02-11 18:58:49 +0600

    bew change

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
       MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-          backgroundColor: Colors.blueAccent,
+          backgroundColor: Colors.greenAccent,
           appBar: AppBar(
             title: const Text('Dice'),
             backgroundColor: Colors.indigo,
