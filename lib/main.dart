9956309ff36e1df8ba294b82147f2894d8ad729e
itsirajul <shirajulislamparvez@gmail.com>
2022-02-22 09:09:13 +0600

    daily commit

--- lib/main.dart
+++ lib/main.dart
@@ -33,14 +33,7 @@ class DicePage extends StatelessWidget {
            ),
           ),
 
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                print('Right Button ');
-              },
-              child: Image.asset('images/dice1.png'),
-            ),
-          ),
+
 
           Expanded(
             child: FlatButton(
