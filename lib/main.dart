57f761ded7b188a3ffd3e419cbc984be70986115
itsirajul <shirajulislamparvez@gmail.com>
2022-03-01 19:30:02 +0600

    Remove Extra Button

--- lib/main.dart
+++ lib/main.dart
@@ -41,25 +41,6 @@ class DicePage extends StatelessWidget {
               child: Image.asset('images/dice1.png'),
             ),
           ),
-
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                print('Right Button ');
-              },
-              child: Image.asset('images/dice1.png'),
-            ),
-          ),
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                print('Right Button ');
-              },
-              child: Image.asset('images/dice1.png'),
-            ),
-          ),
-
-
         ],
       ),
     );
